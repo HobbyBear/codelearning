f823e013debdb249e3c5755b0b00bcab8d32226e
xiongch <xiongch@flatincbr.com>
2023-06-20 14:25:53 +0800

    consistent hash

---
+++ consistenthash/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"hash/crc32"
+	"sort"
+)
+
+func main() {
+	ch := NewConsistentHash(3)
+	ch.AddNodes("node1")
+	ch.AddNodes("node2")
+	ch.AddNodes("node3")
+	fmt.Println(ch.GetNode("lanpangzi"))
+}
+
+type ConsistentHash struct {
+	nodes      map[uint32]string
+	keys       []uint32
+	replicates int
+}
+
+func NewConsistentHash(replicate int) *ConsistentHash {
+	return &ConsistentHash{
+		nodes:      make(map[uint32]string),
+		keys:       make([]uint32, 0),
+		replicates: replicate,
+	}
+}
+
+func (c *ConsistentHash) AddNodes(node string) {
+	for i := 0; i <= c.replicates; i++ {
+		nodename := fmt.Sprintf("%s#%d", node, i)
+		hashKey := crc32.ChecksumIEEE([]byte(node))
+		c.nodes[hashKey] = nodename
+		c.keys = append(c.keys, hashKey)
+	}
+	sort.Slice(c.keys, func(i, j int) bool {
+		return c.keys[i] < c.keys[j]
+	})
+}
+
+func (c *ConsistentHash) GetNode(key string) string {
+	hashKey := crc32.ChecksumIEEE([]byte(key))
+	nodekeyIndex := sort.Search(len(c.keys), func(i int) bool {
+		return c.keys[i] >= hashKey
+	})
+	if nodekeyIndex == len(c.keys) {
+		nodekeyIndex = 0
+	}
+	return c.nodes[c.keys[nodekeyIndex]]
+}
