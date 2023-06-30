db2a4689c86c49aff5770da0bd2e42e8c8a879e7
xiongch <xiongch@flatincbr.com>
2023-06-30 14:53:07 +0800

    fix io.Copy

--- layer4balance/proxy/proxy_server.go
+++ layer4balance/proxy/proxy_server.go
@@ -2,7 +2,6 @@ package proxy
 
 import (
 	"codelearning/balancepolicy"
-	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -31,73 +30,24 @@ func (s *Server) Run() {
 			}
 			fmt.Println("获取到了新连接", remoteAddr, backendIp)
 			go func() {
-				_, err := copyBuffer(serverConn, c)
+				_, err := io.Copy(serverConn, c)
 				if err != nil {
-					c.Close()
-					serverConn.Close()
-					fmt.Println(err)
-					return
+					fmt.Println(err, 1)
 				}
-				fmt.Println("结束1")
+				c.Close()
+				serverConn.Close()
+				fmt.Println("结束1", err)
 			}()
 			go func() {
-				_, err := copyBuffer(c, serverConn)
+				_, err := io.Copy(c, serverConn)
 				if err != nil {
-					c.Close()
-					serverConn.Close()
-					fmt.Println(err)
-					return
+					fmt.Println(err, 2)
 				}
-				fmt.Println("结束2")
+				c.Close()
+				serverConn.Close()
+				fmt.Println("结束2", err)
 			}()
 		}(c)
 	}
 
 }
-
-func copyBuffer(dst io.Writer, src io.Reader) (written int64, err error) {
-	var buf []byte
-	if buf == nil {
-		size := 32 * 1024
-		if l, ok := src.(*io.LimitedReader); ok && int64(size) > l.N {
-			if l.N < 1 {
-				size = 1
-			} else {
-				size = int(l.N)
-			}
-		}
-		buf = make([]byte, size)
-	}
-	for {
-		nr, er := src.Read(buf)
-
-		if nr > 0 {
-			nw, ew := dst.Write(buf[0:nr])
-			if nw < 0 || nr < nw {
-				nw = 0
-				if ew == nil {
-					ew = errInvalidWrite
-				}
-			}
-			written += int64(nw)
-			if ew != nil {
-				err = ew
-				break
-			}
-			if nr != nw {
-				err = ErrShortWrite
-				break
-			}
-		}
-		if er != nil {
-			err = er
-			break
-		}
-	}
-	return written, err
-}
-
-var ErrShortWrite = errors.New("short write")
-
-// errInvalidWrite means that a write returned an impossible count.
-var errInvalidWrite = errors.New("invalid write result")
