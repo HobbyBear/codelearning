7af94c25df748f046673c9af52a3ea5cb4468a83
xiongch <xiongch@flatincbr.com>
2023-09-06 18:10:58 +0800

    add heap

--- heap/heap_test.go
+++ heap/heap_test.go
@@ -7,7 +7,7 @@ import (
 
 func TestHeapify(t *testing.T) {
 	arr := []int{10, 100, 90, 1, 4, 20, 8}
-	h := Heapify(arr)
+	h := HeapInsert(arr)
 	fmt.Println(h.arr)
 	for i := 0; i < len(arr); i++ {
 		fmt.Println(h.Pop())
